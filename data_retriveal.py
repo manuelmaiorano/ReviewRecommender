d3098188e2380c987616d829352578a0587123a0
Manuel Maiorano <manuelmaiorano01@gmail.com>
2022-11-27 12:27:42 +0100

    Completed api of data_retriveal
    Added methods to iterate through pull requests and commits and get files
    from each. Removed pydriller dependency for the moment.

--- data_retriveal.py
+++ data_retriveal.py
@@ -1,60 +1,103 @@
-import pydriller
 import requests
-import json
 from dataclasses import dataclass
+from datetime import datetime
+import pytz
+import base64
 
 class RepoRetriveal:
     @dataclass
     class PullRequest:
         number: int
-        author: str
-        files: list[str]
+        author_login: str
         commenters: list[str]
-        date: str
+        date: datetime
+
+    @dataclass
+    class Commit:
+        sha: str
+        author_login: str
+        files: list[str]
+        date: datetime
     
-    def __init__(self, owner, repo, token):
+    def __init__(self, owner, repo, token=None):
         self.owner = owner
         self.repo = repo
         self.token = token
         self.base_url = f'https://api.github.com/repos/{owner}/{repo}'
         self.pydriller_url = f'https://github.com/{owner}/{repo}.git'
+        self.headers = {"Accept": "application/vnd.github+json"}
+        if token: self.headers["Authorization"] = f"Bearer {token}"
+
+    def getFromUrl(self, url, params=None):
+        r = requests.get(url, headers=self.headers, params=params)
+        r.raise_for_status()
+        return r.json()
 
     def getPullByNumber(self, number):
         pull_url = f'{self.base_url}/pulls/{str(number)}'
-        files_url = f'{pull_url}/files'
         comments_url = f'{pull_url}/comments'
 
-        headers={"Accept": "application/vnd.github+json", "Authorization": "Bearer %s"%self.token}
-
-        r = requests.get(pull_url, headers=headers)
-        data = r.json()
-
+        data = self.getFromUrl(pull_url)
         author_login = data['user']['login']
-        author_url = f'https://api.github.com/users/{author_login}'
-        date = data['created_at']
+        date_str = data['created_at']
+        date = datetime.fromisoformat(date_str[:-1])
+        date = pytz.utc.localize(date)
 
-        r = requests.get(author_url, headers=headers)
-        data = r.json()
-        author = data['name']
-
-        r = requests.get(comments_url, headers=headers)
-        data = r.json()
-
+        data = self.getFromUrl(comments_url)
         commenters = []
         for comment in data:
             commenters.append(comment['user']['login'])
 
+        return RepoRetriveal.PullRequest(number, author_login, commenters, date)
 
-        r = requests.get(files_url, headers=headers)
-        data = r.json()
+    def getCommitBySha(self, sha):
+        commit_url = f'{self.base_url}/commits/{sha}'
 
-        file_names = []
-        for file in data:
-            file_names.append(file["filename"])
+        data = self.getFromUrl(commit_url)
+        author_login = data['author']['login']
+        date_str = data['commit']['author']['date']
+        date = datetime.fromisoformat(date_str[:-1])
+        date = pytz.utc.localize(date)
 
-        return RepoRetriveal.PullRequest(number, author, file_names, commenters, date)
+        files = []
+        for item in data['files']:
+            files.append(item['filename'])
 
-    def getCommitsIterable(self, to):
-        return pydriller.Repository(path_to_repo=self.pydriller_url,
-                                    only_no_merge=True,
-                                    to=to).traverse_commits()
+        return RepoRetriveal.Commit(sha, author_login, files, date)
+
+    def getCommitsIterable(self, toDate: datetime):
+        commit_url = f'{self.base_url}/commits'
+        date_str = toDate.isoformat()[:-6]+'Z'
+        data = self.getFromUrl(commit_url, params={'since': date_str})
+
+        for item in data:
+            yield self.getCommitBySha(item['sha'])
+
+    def getPullIterable(self, toNumber):
+        pull_url = f'{self.base_url}/pulls'
+        data = self.getFromUrl(pull_url, params={'state': 'closed'})
+
+        for item in data:
+            if item['number'] < toNumber:
+                yield self.getPullByNumber(item['number'])
+
+
+    def getPullFiles(self, pull: PullRequest):  
+        files_url = f'{self.base_url}/pulls/{str(pull.number)}/files'
+        data = self.getFromUrl(files_url)
+        return self.getFileContentList(data)
+
+    def getCommitFiles(self, commit: Commit):
+        commit_url = f'{self.base_url}/commits/{commit.sha}'
+        data = self.getFromUrl(commit_url)
+        return self.getFileContentList(data['files'])
+
+    def getFileContentList(self, files_data):
+        files = []
+        for item in files_data:
+            content_url = item['contents_url']
+            file_data = self.getFromUrl(content_url)
+            content = file_data['content']
+            files.append(base64.b64decode(content).decode('utf-8'))
+        
+        return files
