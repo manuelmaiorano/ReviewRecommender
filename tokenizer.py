6c399e6be8293846797669e9638b6d25421744e5
Manuel Maiorano <manuelmaiorano01@gmail.com>
2022-12-16 20:11:07 +0100

    Refactor tokenizer api

--- tokenizer.py
+++ tokenizer.py
@@ -1,39 +1,28 @@
 import re
+from collections import defaultdict
+from data_retriveal import RepoRetriveal
 
 class Tokenizer:
     
     @staticmethod
-    # conta le occorrenze dei metodi
-    def method_occurrences(file_list):
-        method_pattern = re.compile(r'def\s+(\w+)\(')
-        method_dict = {}
-        for file in file_list:
-            file_content = file.content
-            for method_name in method_pattern.findall(file_content):
-                if method_name in method_dict:
-                    method_dict[method_name] += 1
-                else:
-                    method_dict[method_name] = 1
-        return method_dict
-
-    @staticmethod
-    # conta le occorrenze delle classi
-    def class_occurrences(file_list):
+    def getTokenFreqs(file_list: list[RepoRetriveal.RepoFile]):
+        import_pattern = re.compile('import +(.*?)[ \r\n]')
         class_pattern = re.compile(r'class\s+(\w+)(?:\(.*\))?\s?:')
-        class_dict = {}
+        tokenFreqs = defaultdict(lambda: 0)
         for file in file_list:
-            file_content = file.content
-            for class_name in class_pattern.findall(file_content):
-                if class_name in class_dict:
-                    class_dict[class_name] += 1
-                else:
-                    class_dict[class_name] = 1
-        return class_dict
+            tokens = file.filepath.split('/')
+            for token in tokens:
+                tokenFreqs[token] += 1
 
-    @staticmethod
-    # conta le occorrenze delle classi e dei metodi
-    def class_method_occurrences(file_list):
-        method_dict = Tokenizer.method_occurrences(file_list)
-        class_dict = Tokenizer.class_occurrences(file_list)
-        all_dict = {**method_dict, **class_dict}
-        return all_dict
+            if not file.filepath.endswith('.py'): continue
+
+            matches = import_pattern.findall(file.content)
+            for match in matches:
+                for token in match.split('.'):
+                    tokenFreqs[token] += 1
+            
+            matches = class_pattern.findall(file.content)
+            for match in matches:
+                tokenFreqs[match] += 1
+
+        return tokenFreqs
