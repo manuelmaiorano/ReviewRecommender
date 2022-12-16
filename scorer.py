161a17f97fcbc6f9beca336a3214ed53bee2747f
Manuel Maiorano <manuelmaiorano01@gmail.com>
2022-12-16 20:12:05 +0100

    Add print function scorer

--- scorer.py
+++ scorer.py
@@ -9,6 +9,29 @@ class Scorer:
         self.reviewerToScore[reviewer] += score
 
     def getSorted(self):
-        reviewer = dict(sorted(self.reviewerToScore.items(), key=lambda item: item[1]))
+        reviewer = dict(sorted(self.reviewerToScore.items(), 
+                               key=lambda item: item[1], 
+                               reverse=True))
         return reviewer
+
+    def prettyFormat(self):
+        totalScore = 0
+        for score in self.reviewerToScore.values(): totalScore += score
+
+        finalString = 'Reviewer         | Score      ' + '\n'
+        finalString += '-----------------------------' + '\n'
+        for reviewer, score in self.getSorted().items():
+            spaces = ' ' * (len('Reviewer         ') - len(reviewer))
+            finalString += (reviewer + spaces + '|' + f'{score/totalScore*100: .2f} %' + '\n')
+
+        return finalString
+
+if __name__ == '__main__':
+    scorer = Scorer()
+    scorer.addReviewerScore('man1', 23.4545)
+    scorer.addReviewerScore('mano0o0', 245.45)
+    scorer.addReviewerScore('manko1', 2.4545)
+    scorer.addReviewerScore('manui', 23.445)
+    print(scorer.prettyFormat())
+
 
