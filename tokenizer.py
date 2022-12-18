bceed2e9d08c4a636adbee210a92fc21eab53bb9
Manuel Maiorano <manuelmaiorano01@gmail.com>
2022-12-18 17:13:00 +0100

    Improve tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -4,10 +4,13 @@ from collections import defaultdict
 from data_retriveal import RepoRetriveal
 
 class Tokenizer:
+
+    PHYTHONREGEX = r"^\s*from\s+(\w+)\s+import\s+([\w\.]+(?:\s*,\s*\w+)*)|\
+                    ^\s*import\s+([\w\.]+(?:\s*,\s*\w+)*)"
     
     @staticmethod
     def getTokenFreqs(file_list: list[RepoRetriveal.RepoFile]):
-        import_pattern = re.compile(r'^\s*(?:from|import)\s+([\w\.]+(?:\s*,\s*\w+)*)', re.MULTILINE)
+        import_pattern = re.compile(Tokenizer.PHYTHONREGEX, re.MULTILINE)
         tokenFreqs = defaultdict(lambda: 0)
         for file in file_list:
             tokens = file.filepath.split('/')
@@ -17,8 +20,14 @@ class Tokenizer:
             if not file.filepath.endswith('.py'): continue
 
             matches = import_pattern.findall(file.content)
+            listTokens = []
             for match in matches:
-                for token in match.split('.'):
-                    tokenFreqs[token] += 1
+                for token in match:
+                    if not token: continue
+                    listTokens.extend(re.split(r'\W+', token)) 
+            
+            for token in listTokens:
+                if not token: continue
+                tokenFreqs[token] += 1
 
         return tokenFreqs
