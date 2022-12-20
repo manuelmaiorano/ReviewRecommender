616448eedbede5bd31fd596f918a481b2d88f042
Manuel Maiorano <manuelmaiorano01@gmail.com>
2022-12-20 08:38:03 +0100

    Modify tokenizer tests

--- test/tokenizer/test_tokenizer.py
+++ test/tokenizer/test_tokenizer.py
@@ -20,9 +20,31 @@ class TestTokenizer:
 
     def test_tokenizer(self):
         tokenFreqs = Tokenizer.getTokenFreqs(self.file_list)
-        expected_output = {'test': 3, 'tokenizer': 3, 'data': 3, 'file1.py': 1,
-                            'file2.py': 1, 'typing': 2, 'Dict': 2, 'Iterator': 2, 
-                            'file3.py': 1, '__future__': 1, 'annotations': 1, 
-                            'contextlib': 1, 'contextmanager': 1, 'nullcontext': 1, 
-                            'Tuple': 1, 'typing_extensions': 1, 'TypeAlias': 1}
+        expected_output = {'test': 3, 'tokenizer': 3, 'data': 3, 
+                           'file1': 1, 'os': 1, 'pytest': 1, 'file2': 1, 
+                           'contextlib': 2, 'typing': 2, 'Dict': 2, 'Iterator': 2, 
+                           'file3': 1, '__future__': 1, 'annotations': 1, 
+                           'contextmanager': 1, 'nullcontext': 1, 'Tuple': 1, 
+                           'typing_extensions': 1, 'TypeAlias': 1}
+        assert tokenFreqs == expected_output
+    
+    def test_other_languages(self):
+        JAVA_FILE = """
+                import packageName.className;
+                package  myPackage;
+        """
+        C_FILE = """
+                #include <iostream>
+                #include "stdlib.h"
+                #include "mylib/file.h"
+        """
+        filejava = RepoRetriveal.RepoFile('filej.java', JAVA_FILE)
+        filec = RepoRetriveal.RepoFile('filec.cpp', C_FILE)
+
+        tokenFreqs = Tokenizer.getTokenFreqs([filejava, filec])
+        expected_output = {'filej': 1, 'packageName': 1, 
+                           'className': 1, 'myPackage': 1, 
+                           'filec': 1, 'iostream': 1, 'stdlib': 1, 
+                           'mylib': 1, 'file': 1}
+        
         assert tokenFreqs == expected_output
